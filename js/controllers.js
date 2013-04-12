e5df715fa781d8734107cb8ef04b94a633f0a0b5
Nolan Lawson <nolan.lawson@gmail.com>
2013-04-12 08:53:16 +0200

    simplify tab code

--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,6 @@ function ImperialController($scope, $location) {
 	
 	$scope.tabs = [{id : 'home', title : 'Home'},{id : 'about', title : 'About'}, {id : 'contact', title: 'Contact'}]
 	$scope.selectedTabId = $location.path() === '/contact' ? 'contact' : $location.path() === '/about' ? 'about' : 'home';
-    
-    $scope.selectTab = function(tabId) {
-        $scope.selectedTabId = tabId;
-    };
 }
 
 ImperialController.$inject = ['$scope', '$location'];
