133e6029381a3784cee3a1a2255cc5b90297b605
Nolan Lawson <nolan.lawson@gmail.com>
2013-04-12 12:11:50 +0200

    complete rewrite in AngularJS

--- js/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('imperial', []).
-  config(['$routeProvider', function($routeProvider) {
-	    $routeProvider.
-	          when('/home', {templateUrl : 'partials/home.html'}).
-	          when('/about', {templateUrl : 'partials/about.html'}).
-	          when('/contact', {templateUrl : 'partials/contact.html'}).
-		      otherwise({redirectTo: '/home'});
-  }]);
