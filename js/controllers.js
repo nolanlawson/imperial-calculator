0d5086e64d3abb6c146a34b7f6c48cb197aa61ef
Nolan Lawson <nolan.lawson@gmail.com>
2013-04-12 21:55:07 +0200

    add feature to show who is in the lead

--- js/controllers.js
+++ js/controllers.js
@@ -15,6 +15,8 @@ function ImperialController($scope, $location) {
 	
 	$scope.possibleMultipliers = [0,1,2,3,4,5];
 	
+	$scope.players = [];
+	
 	$scope.countries = [
         {id: 'at', name: 'Austria-Hungary'},
         {id: 'it', name: 'Italy'},
@@ -35,14 +37,34 @@ function ImperialController($scope, $location) {
         }
     }
     
-    $scope.players = [];
+    //
+    // Watches
+    //
+    
+    var watchExpression = 'players[0].cash + players[1].cash + players[2].cash + players[3].cash + players[4].cash + players[5].cash + ' +
+            'countries[0].multiplier + countries[1].multiplier + countries[2].multiplier + countries[3].multiplier + countries[4].multiplier + countries[5].multiplier + ' +
+            'players[0].shares.length + players[1].shares.length + players[2].shares.length + players[3].shares.length + players[4].shares.length + players[5].shares.length';
+    
+    $scope.$watch(watchExpression, function() {
+        for (var i = 0; i < $scope.players.length; i++) {
+            var player = $scope.players[i];
+            
+            player.score = calculateScore(player);
+        }
+        updateRanking();
+    });
+    
+    //
+    // click/change functions
+    //
     
     $scope.addPlayer = function(){
         $scope.players.push({
             id     : $scope.players.length,
             name   : '',
             shares : [],
-            cash   : ''
+            cash   : '',
+            rank   : {unranked : true}
         });
     };
     
@@ -78,7 +100,12 @@ function ImperialController($scope, $location) {
         // else do nothing; someone else owns it
     };
     
-    $scope.calculateScore = function(player) {
+    
+    //
+    // Internally used functions
+    //
+    
+    function calculateScore(player) {
         var sum = player.cash || 0;
         for (var i = 0; i < player.shares.length; i++) {
             var share = player.shares[i];
@@ -86,7 +113,91 @@ function ImperialController($scope, $location) {
             sum += share.value * share.country.multiplier;
         }
         return sum;
-    };
+    }
+    
+    // returns an object containing "first" or "last", depending on the number of players
+    // TODO: optimize this
+    function updateRanking() {
+        
+        var i;
+        
+        // clear present values
+        for (i = 0; i < $scope.players.length; i++) {
+            $scope.players[i].rank = {unranked : true};
+        }
+        
+        if ($scope.players.length < 2) {
+            // no interest in calculating this yet
+            return;
+        }
+        
+        // count unique scores
+        var uniqueScores = {};
+        for (i = 0; i < $scope.players.length; i++) {
+            uniqueScores[$scope.players[i].score] = true;
+        }
+        
+        var uniqueScoreCount = 0;
+        for (var uniqueScore in uniqueScores) {
+            if (uniqueScores.hasOwnProperty(uniqueScore)) {
+                uniqueScoreCount++;
+            }
+        }
+        
+        if (uniqueScoreCount === 1) {
+            // everyone has the same score - not interesting!
+            return;
+        }
+        
+        // figure out top and bottom scores to deal with ties
+        var topScore = -1;
+        var bottomScore = Number.MAX_VALUE;
+        for (i = 0; i < $scope.players.length; i++) {
+            var score = $scope.players[i].score;
+            if (score > topScore) {
+                topScore = score;
+            }
+            if (score < bottomScore) {
+                bottomScore = score;
+            }
+        }
+        
+        // copy
+        var rankedPlayers = [];
+        for (i = 0; i < $scope.players.length; i++) {
+            rankedPlayers.push($scope.players[i]);
+        }
+
+        // rank
+        rankedPlayers.sort(function(left, right){ 
+            return right.score - left.score;
+        });
+        
+        // update the "rank" object
+        var lastRankedIndex = -1;
+        for (i = 0; i < rankedPlayers.length; i++) {
+            var player = rankedPlayers[i];
+            player.rank.unranked = false;
+            
+            // determine rank order while maintaining ties
+            if (player.score === topScore) {
+                // first
+                player.rank.first = true;
+                player.rank.order = 0;
+            } else if (player.score === bottomScore) {
+                // last
+                if (lastRankedIndex === -1) {
+                    lastRankedIndex = i;
+                }
+                player.rank.last = true;
+                player.rank.order = lastRankedIndex;
+            } else {
+                // middle
+                player.rank.order = i;
+                player.rank.middle = true;
+            }
+        }
+    }
 }
 
 ImperialController.$inject = ['$scope', '$location'];
