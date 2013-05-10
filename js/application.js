61e26294334a5f1c41e92b50b0749a4b20431e30
Nolan Lawson <nolan.lawson@gmail.com>
2013-05-10 16:05:49 +0200

    improve method for determining window width

--- js/application.js
+++ js/application.js
@@ -1,15 +1,25 @@
 /*
  * Main Angular definitions for the Imperial Score Calculator
  */
-/*global angular navigator*/
+/*global angular window document*/
 (function() {
 
     "use strict";
 
+    function getWindowWidth() {
+        var w = window,
+            d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            x = w.innerWidth || e.clientWidth || g.clientWidth;
+            
+        return x;
+    }
+
     angular.module('imperial', []).
     config(['$routeProvider', function($routeProvider) {
 
-        var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
+        var isMobile = getWindowWidth() <= 767; // same rules as bootstrap
         var homePage = isMobile ? 'partials/home_mobile.html' : 'partials/home.html';
 
         $routeProvider.
